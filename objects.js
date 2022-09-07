d42c9928ae70f8042ec9c9728be9b4b9e729e09c
Viktor Stepanov <vitya.zap@gmail.com>
2022-09-07 17:45:10 +0300

    Add constructor function

--- objects.js
+++ objects.js
@@ -20,6 +20,7 @@ const phone = {
   test: 'test',
 };
 
+// CRUD!!!
 // CREATE
 // const obj1 = {}; // literal, best
 // const obj1 = Object(); // Bad
@@ -37,3 +38,69 @@ phone.color = 'red';
 phone.battery = 4000;
 // DELETE
 delete phone.test;
+
+//constructor function, starts from capital letter
+const User = function (name, surname, age) {
+  this.name = name;
+  this.surname = surname;
+  this.age = age;
+  // assume that function is the same for all users
+  this.brushTeeth = function () {
+    console.log('In progress...');
+  };
+  return NaN; // return can't work here
+};
+
+const user001 = new User('Vik', 'S', undefined);
+const user002 = new User('Viktor', 'U3', 69420);
+const user003 = new User('U3erKa', 'Stepanov', 20);
+const user004 = new User('U3erKa', 'U3', 69420);
+
+const str = `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores eligendi quae
+nemo reiciendis commodi, in exercitationem, molestias, labore modi nisi doloremque
+minus architecto quos nam. Ut voluptatibus repudiandae exercitationem quibusdam.`; // template string
+
+const number1 = 10;
+const number2 = 20;
+// const sum = number1 + number2;
+if (isNaN(number1 - number2)) {
+  console.log('ERROR!');
+} else {
+  console.log(`${number1} * ${number2} = ${number1 * number2}`);
+  console.log(
+    `${number1} > ${number2} ? ${number1 > number2 ? 'true' : 'false'}`
+  );
+}
+
+/*
+const user001 = {
+  name: 'Vik',
+  surname: 'S.',
+  age: undefined,
+  brushTeeth: function () {
+    console.log('Done!');
+  },
+};
+
+const user002 = {
+  name: 'Viktor',
+  surname: 'Stepanov',
+  age: 20,
+  brushTeeth: function () {
+    for (let i = 0; i < 100; i++) {
+      console.log('Brushing in progress');
+    }
+    console.log('Done!');
+    return true;
+  },
+};
+
+const user003 = {
+  name: 'Aster',
+  surname: 'Win Ding',
+  age: -1,
+  brushTeeth: function () {
+    console.log(';');
+  },
+};
+*/
---
+++ task-objects.js
@@ -0,0 +1,12 @@
+const ChatMsg = function (author, text, likes, dislikes) {
+  this.author = author;
+  this.text = text;
+  this.likes = likes;
+  this.dislikes = dislikes;
+  this.editMsg = function () {
+    prompt('Enter your message');
+  };
+};
+
+const msg001 = new ChatMsg('Viktor', 'Lorem ipsum dolor sit amet consectetur adipisicing.', 25, 4);
+const msg002 = new ChatMsg('U3erKa', 'Fugiat error quidem deleniti ab similique!', 999, 1);
