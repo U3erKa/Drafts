1e78058161ec5f7ee1861eb9c690e207aa145646
Viktor Stepanov <vitya.zap@gmail.com>
2022-09-12 20:39:40 +0300

    Add prototypes chain

---
+++ inheritance.js
@@ -0,0 +1,45 @@
+'use strict';
+function Phone(model, manufacturer) {
+  this.model = model;
+  this.manufacturer = manufacturer;
+}
+
+// phone1.call === phone2.call is true
+const phoneProto = {
+  isOn: false,
+  call: function (number) {
+    console.log(`${this.manufacturer} ${this.model} is caling ${number}`);
+  },
+  // __proto__: deviceProto,
+};
+
+const deviceProto = {
+  turnOn: function () {
+    console.log('device is turned ON!');
+  },
+};
+
+Phone.prototype = phoneProto;
+
+const phone1 = new Phone('Pro', 'Android');
+const phone2 = new Phone('Lite', 'Xiaomi');
+
+// const phone1 = {
+//   model: 'Pro',
+//   manufacturer: 'Android',
+// };
+
+// const phone2 = {
+//   model: 'Lite',
+//   manufacturer: 'Xiaomi',
+// };
+
+// phone1.__proto__ = phoneProto;
+// phone2.__proto__ = phoneProto;
+// phoneProto.__proto__ = deviceProto;
+
+// phone1.call === phone2.call is false
+
+// arr1.push === arr2.push is true, prototype is array
+const arr1 = [1];
+const arr2 = [1];
