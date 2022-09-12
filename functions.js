1ed2d35b6649b0030729bfa66efd3f3ead7799e8
Viktor Stepanov <vitya.zap@gmail.com>
2022-09-12 18:49:25 +0300

    'use strict';

--- functions.js
+++ functions.js
@@ -1,10 +1,19 @@
-// functional expression
-const input1 = prompt('Enter 1st number');
-const input2 = prompt('Enter 2nd number');
+'use strict';
 
+// varrr = 10; // error if 'use strict'
+
+// functional expression
+// const input1 = prompt('Enter 1st number');
+// const input2 = prompt('Enter 2nd number');
+const input1 = 1;
+const input2 = 3;
+
+// console.log(getSumOfTwo(2, 5)); // breaks
+// console.log(getSumOfTwo2(2, 5));
+
+// expression
 const getSumOfTwo = function (num1, num2) {
   const result = +num1 + Number(num2);
-
   return result;
   // const message = num1 + ' + ' + num2 + ' = ' + result;
 
@@ -12,6 +21,13 @@ const getSumOfTwo = function (num1, num2) {
   // console.log(message);
 };
 
+// declaration
+function getSumOfTwo2(num1, num2) {
+  const result = +num1 + Number(num2);
+  this.alert = 10; // can break Window.alert >:)
+}
+
+const test123 = new getSumOfTwo2();
 const result = getSumOfTwo(input1, input2);
 console.log(result);
 
