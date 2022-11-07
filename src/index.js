a91a2449aa33798303166977b987459c10da1ffe
Viktor Stepanov <vitya.zap@gmail.com>
2022-11-07 21:15:12 +0200

    Add Header and Greeting components

--- src/index.js
+++ src/index.js
@@ -1,13 +1,56 @@
+// @ts-check
+'use strict';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+import PropTypes from 'prop-types';
 
+// @ts-expect-error
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const name = 'U3erKa';
+const user = {
+  name: 'user',
+  profilePicSrc:
+    'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png',
+};
+
+// function Header(props) {
+//   return (
+//     <header>
+//       <h1>My react site</h1>
+//       <img src={props.src} alt={props.name} />
+//     </header>
+//   );
+// }
+
+class Header extends React.Component {
+  render() {
+    // const { name } = this.props;
+    return (
+      <header>
+        <h1>My site</h1>
+        <img src={user.profilePicSrc} alt={user.name} />
+      </header>
+    );
+  }
+}
+
+function Greeting(props) {
+  const { name } = props;
+  return <p>Hello, {name}!</p>;
+}
+Greeting.propTypes = {
+  name: PropTypes.string.isRequired,
+};
+
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Header />
+    <Greeting name={user.name} />
+    <Greeting name={name} />
   </React.StrictMode>
 );
 
@@ -15,3 +58,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
