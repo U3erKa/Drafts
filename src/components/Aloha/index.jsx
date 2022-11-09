4d36f6c90713833e448dfe3b3363ecc0a7dbc145
Viktor Stepanov <vitya.zap@gmail.com>
2022-11-09 20:56:49 +0200

    Add parents and children relations examples

--- src/components/Aloha/index.jsx
+++ src/components/Aloha/index.jsx
@@ -9,24 +9,31 @@ class Aloha extends Component {
       clicks: 0,
     };
   }
+
   handleClick = () => {
     this.setState({ isGreeting: !this.state.isGreeting });
   };
 
   render() {
-    const { name } = this.props;
+    const { name, makeFavourite, id } = this.props;
+    // const { name, func } = this.props;
     const { isGreeting } = this.state;
     return (
-      <>
+      <div>
         <p>
           {isGreeting ? 'Hello' : 'Bye'}, {name}!
         </p>
         <button onClick={this.handleClick}>Change Mode</button>
-      </>
+        {/* <button onClick={func}>Exec func</button> */}
+        <button onClick={() => makeFavourite(id)}>Toggle favourite</button>
+      </div>
     );
   }
   static propTypes = {
-    name: PropTypes.string,
+    name: PropTypes.string.isRequired,
+    func: PropTypes.func,
+    makeFavourite: PropTypes.func,
+    id: PropTypes.number.isRequired,
   };
 }
 
--- src/index.js
+++ src/index.js
@@ -1,33 +1,35 @@
 // @ts-check
-'use strict';
+// 'use strict';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Header from './components/Header';
-import Greeting from './components/Greeting';
-import Aloha from './components/Aloha';
+// import Header from './components/Header';
+// import Greeting from './components/Greeting';
+// import Aloha from './components/Aloha';
+import AlohaList from './components/AlohaList';
+// import App from './components/App';
 
 // @ts-expect-error
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const name = 'U3erKa';
-const user = {
-  name: 'user',
-  profilePicSrc: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png',
-};
+// const name = 'U3erKa';
+// const user = {
+//   name: 'user',
+//   profilePicSrc: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png',
+// };
 
 root.render(
   <>
     {/* <App /> */}
-    <Header name={user.name} profilePicSrc={user.profilePicSrc} />
+    {/* <Header name={user.name} profilePicSrc={user.profilePicSrc} />
     <Greeting name={user.name} />
     <Greeting name={name} id={69} />
     <Aloha name={'U2'} isGreeting={true} />
-    <Aloha name={'U3'} isGreeting={false} />
+    <Aloha name={'U3'} isGreeting={false} /> */}
     {/* @ts-ignore */}
-    <input disabled={user.name !== user} />
-
+    {/* <input disabled={user.name !== user} /> */}
+    <AlohaList />
   </>
 );
 
