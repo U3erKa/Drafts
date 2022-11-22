1d415c81b496a6fd10b9d487294d23d26380f23b
Viktor Stepanov <vitya.zap@gmail.com>
2022-11-23 00:47:22 +0200

    Initial commit

---
+++ src/practice-constructor-prototypes.ts
@@ -0,0 +1,72 @@
+const User: any = function (firstName: string, lastName: string, email: string, age: string) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.email = email;
+  this.age = age;
+  this.isLoggedIn = false;
+};
+
+const userProto = {
+  getFullName: function () {
+    console.log(
+      `Info about the user:
+First name: ${this.firstName}
+Last  name: ${this.lastName}`
+    );
+    return `${this.firstName} ${this.lastName}`;
+  },
+  logout: function () {
+    this.isLoggedIn = false;
+    console.log(`${this.firstName} just logged out`);
+  },
+  login: function () {
+    this.isLoggedIn = true;
+    console.log(`${this.firstName} just logged in`);
+  },
+};
+
+User.prototype = userProto;
+
+const users = [
+  new User('U1erKa', 'U1', 'email1@example.com', '18'),
+  new User('U2erKa', 'U2', 'email2@example.com', '19'),
+  new User('U3erKa', 'U3', 'email3@example.com', '20'),
+  new User('U4erKa', 'U4', 'email4@example.com', '21'),
+  new User('U5erKa', 'U5', 'email5@example.com', '22'),
+];
+
+console.log(users);
+
+function allFullNames(index: number) {
+  if (index === undefined) {
+    users.forEach(function (User) {
+      console.log(User.getFullName());
+    });
+  } else {
+    users[index].getFullName();
+  }
+}
+
+function allLogin(index: number) {
+  if (index === undefined) {
+    users.forEach(function (User) {
+      console.log(User.login());
+    });
+  } else {
+    users[index].login();
+  }
+  console.log(users);
+}
+
+function allLogout(index: number) {
+  if (index === undefined) {
+    users.forEach(function (User) {
+      console.log(User.logout());
+    });
+  } else {
+    users[index].logout();
+  }
+  console.log(users);
+}
+
+export { User, allFullNames, allLogin, allLogout };
---
+++ src/practice-recursion.ts
@@ -0,0 +1,27 @@
+function factorialAlt(number: number) {
+  if (typeof number !== 'number' || isNaN(number) || number < 0 || !Number.isInteger(number)) {
+    return NaN;
+  }
+  let result = 1;
+  const recursion = (number: number) => {
+    if (number > 1) {
+      result *= number;
+      recursion(number - 1);
+    }
+  };
+  recursion(number);
+  return result;
+}
+
+const factorial = (number: number): number => {
+  if (typeof number !== 'number' || isNaN(number) || number < 0 || !Number.isInteger(number)) {
+    return NaN;
+  }
+  if (number === 0 || number === 1) {
+    return 1;
+  } else if (number > 1) {
+    return number * factorial(number - 1);
+  }
+};
+
+export { factorialAlt, factorial };
