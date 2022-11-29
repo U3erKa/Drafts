0a3d786dae6dabf58560d4f58436f7e74d9ff92d
Viktor Stepanov <vitya.zap@gmail.com>
2022-11-29 20:33:46 +0200

    Add refs use cases

--- src/components/RefsClicker/index.jsx
+++ src/components/RefsClicker/index.jsx
@@ -1,9 +1,49 @@
 import { useClickerRef } from 'hooks';
-import React, { useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
-export default function RefsClicker() {
-  const myRef = useRef(null);
-  const [click] = useClickerRef(myRef);
+export const RefsClicker = (props) => {
+  const elemRef = useRef();
+  const clicks = useClickerRef(elemRef);
+  const inputRef = useRef();
+  const prevClicks = useRef(clicks);
+  const renders = useRef(1);
+  const [value, setValue] = useState(0);
 
-  return <div ref={myRef}>{click}</div>;
-}
+  useEffect(() => {
+    renders.current = renders.current + 1;
+  });
+
+  useEffect(() => {
+    prevClicks.current = clicks;
+  }, [clicks]);
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [inputRef]);
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  return (
+    <div id="test">
+      <h1>Clicks: {clicks}</h1>
+      <p>Prev clicks {prevClicks.current}</p>
+      <p>Renders {renders.current}</p>
+      <input ref={inputRef} type="text" value={value} onChange={handleChange} />
+      <div
+        ref={elemRef}
+        style={{
+          width: '300px',
+          height: '200px',
+          background: 'limegreen',
+          margin: '20px',
+        }}
+      ></div>
+    </div>
+  );
+};
+
+export default RefsClicker;
