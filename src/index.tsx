26cd6fc42d392942d5bdad524a08d0e2c48eb5e9
Viktor Stepanov <vitya.zap@gmail.com>
2022-12-05 23:57:13 +0200

    Refactor project structure
    Typescript-ify remaining files, move _variables.scss

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,3 @@
-// @ts-check
-// 'use strict';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
@@ -33,7 +31,6 @@ root.render(
     <Greeting name={name} id={69} />
     <Aloha name={'U2'} isGreeting={true} />
     <Aloha name={'U3'} isGreeting={false} /> */}
-    {/* @ts-ignore */}
     {/* <input disabled={user.name !== user} /> */}
     {/* <AlohaList /> */}
     {/* <Clicker /> */}
@@ -45,4 +42,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
