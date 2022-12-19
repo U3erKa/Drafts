cb1cf029f1bf2aefd0fa38e061b446fb39aff14e
Viktor Stepanov <vitya.zap@gmail.com>
2022-12-19 20:20:22 +0200

    Setup basic low-level server

--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-console.log(true);
+// const fs = require('fs/promises');
+const http = require('http');
+
+const PORT = process.env.PORT ?? 3000;
+
+const server = http.createServer(async (req: any, res: any) => {
+  res.writeHead(200, {});
+  res.end('Hello world!');
+});
+
+server.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
+});
