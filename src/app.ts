9c90c4d1096539a1d734a1fa49e24d4212ba1472
Viktor Stepanov <vitya.zap@gmail.com>
2022-12-22 18:38:07 +0200

    Refactor app routes

--- src/app.ts
+++ src/app.ts
@@ -3,16 +3,16 @@ import { bodyParser, validateUser } from './middleware/userMW.js';
 import { addUserToDB, deleteUser, getUser, getUsers, updateUser } from './controller/userController.js';
 
 const app = express();
+// app.use(bodyParser);
+app.use('/users', bodyParser);
 
 app.get('/', async (req, res) => {
   res.status(200).send('Hello world');
 });
 
-app.get('/users', getUsers);
-app.get('/users/:userId', getUser);
-app.post('/users', bodyParser, validateUser, addUserToDB);
-app.put('/users/:userId', bodyParser, validateUser, updateUser);
-app.delete('/users/:userId', deleteUser);
+app.route('/users').get(getUsers).post(validateUser, addUserToDB);
+
+app.route('/users/:userId').get(getUser).put(validateUser, updateUser).delete(deleteUser);
 
 app.get('/test*', async (req, res) => {
   res.status(200).send(`${req.method} ${req.path}`);
