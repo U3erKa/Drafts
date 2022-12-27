e27d2034eecaba56ef7873cdd62ca0d5084c9d3f
Viktor Stepanov <vitya.zap@gmail.com>
2022-12-27 21:03:14 +0200

    Define data integrity constraints

--- create_table.sql
+++ create_table.sql
@@ -2,10 +2,25 @@
 -- DROP TABLE IF EXISTS my_table;
 
 -- NUMERIC(6 + 2, 2)
+-- NOT NULL UNIQUE is like PRIMARY KEY
+-- CHECK insile () boolean logic
+
+  -- TODO RegEx or sth
 CREATE TABLE IF NOT EXISTS cars(
-  model VARCHAR(128),
-  year_of_production INT,
-  price NUMERIC(8, 2),
-  is_used BOOLEAN,
-  created_at DATE
+  -- id UUID PRIMARY KEY DEFAULT gen_randon_uuid (),
+  id SMALLSERIAL PRIMARY KEY,
+  model VARCHAR(128) NOT NULL CHECK (model != ''),
+  year_of_production INT NOT NULL
+  CHECK (year_of_production > 1900 AND year_of_production <= 2023),
+  vin_number INT NOT NULL UNIQUE,
+  price NUMERIC(8, 2) NOT NULL CHECK (price >= 0),
+  is_used BOOLEAN NOT NULL,
+  created_at DATE NOT NULL DEFAULT current_date
+  CHECK (created_at > '1900-01-01' AND created_at <= current_date),
+  updated_at DATE
+  CHECK (updated_at > created_at),
+  UNIQUE (model, year_of_production)
+  -- PRIMARY KEY (model, year_of_production)
 );
+-- 
+DROP TABLE IF EXISTS cars;
--- insert.sql
+++ insert.sql
@@ -1,22 +1,18 @@
-INSERT INTO cars (
-    model,
-    year_of_production,
-    price,
-    is_used,
-    created_at
-  )
-VALUES (
-    'car_model_example',
-    2022,
-    19999.99,
-    true,
-    '2022-02-28'
-  ),(
-    'car_model_example2',
-    2021,
-    9999.99,
-    false,
-    '2022-12-12'
-  );
+-- pass
+INSERT INTO cars 
+(model, year_of_production, vin_number, price, is_used, updated_at)
+VALUES 
+('car_model_example2', 2021, 69421, 9999.99, false, '2023-12-29');
+INSERT INTO cars 
+(model, year_of_production, vin_number, price, is_used, updated_at, created_at)
+VALUES
+('car_model_example', 2022, 69420, 19999.99, true, '2023-03-01', '2021-03-01');
 
--- SELECT * FROM cars;
+-- fail
+INSERT INTO cars 
+(model, year_of_production, vin_number, price, is_used, updated_at)
+VALUES 
+-- ('', 777, 69423, -1850, true, '3002-01-01');
+(' ', 1977, 69423, 20000, true, '2001-01-01');
+-- 
+SELECT * FROM cars;
