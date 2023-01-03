df384a4667eee84433868700a8b4992346f27b1c
Viktor Stepanov <vitya.zap@gmail.com>
2023-01-03 21:07:25 +0200

    Add more select examples

--- sql/DQL.sql
+++ sql/DQL.sql
@@ -3,11 +3,33 @@ SELECT *
 FROM users;
 
 -- @block
+SELECT first_name, birthday, password
+FROM users;
+
+-- @block Very important function XD
+SELECT 2 + 2 "sum";
+
+-- @block
 SELECT *
 FROM users
 WHERE height < 2;
 
 -- @block
-SELECT *, extract (year from age(birthday)) 
+SELECT *, extract (year from age(birthday)) AS age
 FROM users
 WHERE extract (year from age(birthday)) > 30;
+
+-- @block
+SELECT *
+FROM users
+WHERE NOT is_male AND height = 1.50;
+
+-- @block
+SELECT *
+FROM users
+WHERE first_name IN('Victoria', 'Julia');
+
+-- @block
+SELECT *
+FROM users
+WHERE weight BETWEEN 50 AND 90;
