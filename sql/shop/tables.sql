1d0e8b379345fc64e36e10908e31c5ae143627ea
Viktor Stepanov <vitya.zap@gmail.com>
2023-01-03 18:26:46 +0200

    Tweak table creation

--- sql/shop/tables.sql
+++ sql/shop/tables.sql
@@ -1,10 +1,25 @@
+-- @block DROP
+-- Either use CASCADE or delete in particular order
+-- 1 : 1 requires CASCADE
+DROP TABLE IF EXISTS orders_to_products;
+DROP TABLE IF EXISTS orders;
+DROP TABLE IF EXISTS reviews CASCADE;
+DROP TABLE IF EXISTS ratings CASCADE;
+DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS users;
+
 -- @block Users
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
-  name VARCHAR(255) NOT NULL CHECK(name != ''),
+  first_name VARCHAR(255) NOT NULL CHECK(first_name != ''),
+  last_name VARCHAR(255) NOT NULL CHECK(last_name != ''),
   email VARCHAR(255) NOT NULL CHECK(email != ''),
   password VARCHAR(255) NOT NULL CHECK(password != ''),
   phone_num VARCHAR(20) NOT NULL CHECK(phone_num != ''),
+  birthday DATE CHECK(birthday < current_date),
+  is_male BOOLEAN,
+  height numeric(3,2) CHECK (height > 0.2),
+  weigth smallint CHECK (weigth < 1500),
   created_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
   updated_at TIMESTAMP NOT NULL DEFAULT current_timestamp
 );
@@ -16,6 +31,7 @@ CREATE TABLE products (
   description VARCHAR(2047) CHECK(description != ''),
   price NUMERIC(10,2) NOT NULL CHECK(price > 0),
   quantity INT NOT NULL CHECK(quantity >= 0),
+  category VARCHAR(64) NOT NULL CHECK(category != ''),
   manufacturer VARCHAR(255) NOT NULL CHECK(manufacturer != ''),
   min_age SMALLINT CHECK(min_age > 0),
   created_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
@@ -37,7 +53,7 @@ CREATE TABLE reviews (
 CREATE TABLE ratings (
   id SERIAL PRIMARY KEY,
   review_id INT NOT NULL UNIQUE REFERENCES reviews(id),
-  rating NUMERIC(2,1) NOT NULL CHECK(rating >= 0 AND rating <= 10),
+  rating NUMERIC(3,1) NOT NULL CHECK(rating >= 0 AND rating <= 10),
   created_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
   updated_at TIMESTAMP NOT NULL DEFAULT current_timestamp
 );
@@ -61,12 +77,3 @@ CREATE TABLE orders_to_products (
   updated_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
   PRIMARY KEY(order_id, product_id)
 );
-
--- @block
-ALTER TABLE users
-ADD COLUMN birthday date,
-ADD COLUMN height numeric(3,2) CHECK (height > 0.2),
-ADD COLUMN weigth smallint CHECK (weigth < 1500);
-
--- @block DROP
-DROP TABLE users CASCADE;
