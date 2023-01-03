1ecfe6214de51cf566732f2eac09a4eb9b219c6a
Viktor Stepanov <vitya.zap@gmail.com>
2023-01-03 16:55:17 +0200

    Add randomuser.me API

---
+++ api.ts
@@ -0,0 +1,13 @@
+import axios from 'axios';
+import { RandomUserResponse } from './types';
+
+const httpClient = axios.create({
+  baseURL: 'https://randomuser.me/api/',
+});
+
+export const getUsers = async () => {
+  const {
+    data: { results },
+  } = await httpClient.get<RandomUserResponse>('?results=500&seed=123456&page=1');
+  return results;
+};
