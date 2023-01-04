956a42ee4036a0173ff434889dfcb967269f6b14
Viktor Stepanov <vitya.zap@gmail.com>
2023-01-04 11:39:01 +0200

    Add users and products generation config

--- utils.ts
+++ utils.ts
@@ -1,5 +1,11 @@
 import _ from 'lodash';
+import generationConfig from './configs/generation.json' assert { type: 'json' };
 import type { User } from './types';
+
+const {
+  users: { maxHeight, maxWeigth, minHeight, minWeigth },
+  products: { categories, manufacturers, maxPrice, maxQuantity, minPrice, minQuantity, amountProducts },
+} = generationConfig;
 
 function createUserQueryValues(user: User) {
   const {
@@ -18,12 +24,27 @@ function createUserQueryValues(user: User) {
     '${phone}',
     '${birthday}',
     '${gender === 'male'}',
-    '${_.random(1, 2, true)}',
-    '${_.random(40, 150, false)}'
+    '${_.random(minHeight, maxHeight, true)}',
+    '${_.random(minWeigth, maxWeigth, false)}'
+  )`;
+}
+
+function createProductQueryValues(value: unknown, index: number) {
+  return `(
+    'Product ${index}',
+    ${_.random(minPrice, maxPrice, true)},
+    ${_.random(minQuantity, maxQuantity, false)},
+    '${categories[_.random(0, categories.length - 1, false)]}',
+    '${manufacturers[_.random(0, manufacturers.length - 1, false)]}'
   )`;
 }
 
 export const mapUsers = (users: User[]) => {
   const usersElems = users.map(createUserQueryValues);
   return usersElems.join(',');
+};
+
+export const mapProducts = (amountOfProducts = amountProducts) => {
+  const usersElems = new Array(amountOfProducts).fill(undefined).map(createProductQueryValues).join(',');
+  return usersElems;
 };
