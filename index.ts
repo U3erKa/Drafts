ac5578d0088f31ed9ac435d8199a424c9348e153
Viktor Stepanov <vitya.zap@gmail.com>
2023-01-04 11:41:27 +0200

    Create Products model

--- index.ts
+++ index.ts
@@ -1,9 +1,16 @@
 import fs from 'fs/promises';
 import pg from 'pg';
+import _ from 'lodash';
 import config from './configs/DB.json' assert { type: 'json' };
+import generationConfig from './configs/generation.json' assert { type: 'json' };
 import { getUsers } from './api.js';
 import User from './model/User.js';
+import Product from './model/Product.js';
+
 const { Client } = pg;
+const {
+  orders: { maxOrders, minOrders, maxQuantity, minQuantity },
+} = generationConfig;
 
 const client = new Client(config);
 
@@ -17,6 +24,51 @@ const resetDB = await fs.readFile('./sql/shop/tables.sql', 'utf-8');
 await client.query(resetDB);
 
 const createdUsers = await User.bulkCreate(users);
+const products = await Product.bulkCreate();
+
+// создаем заказы
+const ordersString = createdUsers
+  .map((user) =>
+    new Array(_.random(minOrders, maxOrders, false))
+      .fill(undefined)
+      .map(() => `(${user.id})`)
+      .join(',')
+  )
+  .join(',');
+
+const { rows: orders } = await client.query(`
+INSERT INTO orders (
+  user_id
+)
+VALUES ${ordersString}
+RETURNING id;
+`);
+
+// наполнить заказы
+const ordersToProductsString = orders
+  .map((order) => {
+    // отбор продуктов
+    const productsInOrder = new Array(_.random(1, 5))
+      .fill(undefined)
+      .map(() => products[_.random(0, products.length - 1)]);
+
+    // отсортировать повторяющиеся продукты
+    const filteredProducts = [...new Set(productsInOrder)];
+    // вернуть строку типа (1, 4 , 18)
+    return filteredProducts
+      .map((product) => `(${order.id}, ${product.id}, ${_.random(minQuantity, maxQuantity)})`)
+      .join(',');
+  })
+  .join(',');
+
+await client.query(`
+INSERT INTO orders_to_products (
+  order_id,
+  product_id,
+  quantity
+)
+VALUES ${ordersToProductsString};
+`);
 
 await client.end();
 
--- model/User.ts
+++ model/User.ts
@@ -1,19 +1,19 @@
 import pg from 'pg';
 import { mapUsers } from '../utils.js';
+import { TableNames } from '../constants';
 import type { User as UserType } from '../types';
 
 export default class User {
   public static _client: pg.Client;
-  private static _tableName = 'users';
+  private static _tableName = TableNames.USERS;
 
-  static async bulkCreate(users: UserType[]) {
+  public static async bulkCreate(users: UserType[]) {
     const { rows } = await this._client.query(`
-INSERT INTO users
-(first_name, last_name, email, password, phone_num,
-birthday, is_male, height, weight)
-VALUES
-${mapUsers(users)}
-RETURNING *;
+    INSERT INTO ${this._tableName} (
+      first_name, last_name, email, password, phone_num, birthday, is_male, height, weight
+    )
+    VALUES ${mapUsers(users)}
+    RETURNING *;
 `);
     return rows;
   }
