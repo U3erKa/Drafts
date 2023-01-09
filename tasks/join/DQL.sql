fb368a9ee60aeddd0b4ab3258e754a7352d99471
Viktor Stepanov <vitya.zap@gmail.com>
2023-01-09 18:20:50 +0200

    Fix JOIN task solution
    Filter non-unique rows

--- tasks/join/DQL.sql
+++ tasks/join/DQL.sql
@@ -7,12 +7,24 @@ GROUP BY o.id
 ORDER BY o.id ASC
 ;
 -- @block количество категорий в конкретном заказе
-SELECT order_id, count(category)
+SELECT order_id, count(DISTINCT category)
 FROM products p
 JOIN orders_to_products otp ON p.id = product_id
 JOIN orders o ON o.id = order_id
 GROUP BY order_id
 ORDER BY order_id ASC
+;
+-- @block количество категорий в конкретном заказе (sub-query)
+SELECT order_id, count(*)
+FROM (
+  SELECT order_id, category
+  FROM products p
+  JOIN orders_to_products otp ON p.id = product_id
+  JOIN orders o ON o.id = order_id
+  GROUP BY order_id, category
+  ORDER BY order_id ASC
+) AS with_unique_categories
+GROUP BY order_id
 ;
 -- @block данные о товаре и в скольких заказах он есть
 SELECT p.*, count(o.id)
