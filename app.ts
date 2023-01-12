cfa3431707f48d4ebe6f24ffeff63f5016768b99
Viktor Stepanov <vitya.zap@gmail.com>
2023-01-12 21:11:22 +0200

    Setup /users route

--- app.ts
+++ app.ts
@@ -4,4 +4,30 @@ const express = require('express');
 
 const app = express();
 
+const getUsers = async (req: Request, res: Response, next: NextFunction) => {
+  res.send('Hello users');
+};
+
+const validateRequest = async (req: Request, res: Response, next: NextFunction) => {
+  if (true) {
+    return next();
+  }
+  next(req);
+};
+
+const createuser = async (req: Request, res: Response, next: NextFunction) => {
+  res.send(req.body);
+};
+
+const errorHandler = async (err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.log(err);
+  res.send('Error');
+};
+
+app.use(express.json());
+
+app.route('/users').get(getUsers).post(validateRequest, createuser);
+
+app.use(errorHandler);
+
 module.exports = app;
