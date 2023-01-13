aa9f76913bc52e99df45c053c4b4bd750b89450c
Viktor Stepanov <vitya.zap@gmail.com>
2023-01-13 20:22:35 +0200

    Add cars migration

---
+++ migrations/20230113175727-create-car.js
@@ -0,0 +1,55 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('cars', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      model: {
+        allowNull: false,
+        type: Sequelize.STRING(64),
+      },
+      manufacturer: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      modelYear: {
+        type: Sequelize.INTEGER,
+        field: 'model_year',
+      },
+      price: {
+        allowNull: false,
+        type: Sequelize.DECIMAL(9,2),
+      },
+      isUsed: {
+        allowNull: false,
+        type: Sequelize.BOOLEAN,
+        field: 'is_used',
+        defaultValue: false,
+      },
+      serialNumber: {
+        allowNull: false,
+        type: Sequelize.STRING(64),
+        field: 'serial_number',
+        unique: true,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        field: 'created_at',
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        field: 'updated_at',
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('cars');
+  },
+};
