03dad5f4588aaf92f6d7dbdb78289433edb5098d
Viktor Stepanov <vitya.zap@gmail.com>
2023-01-20 16:54:48 +0200

    Add types to model initializers

--- models/car.js
+++ models/car.js
@@ -1,11 +1,15 @@
 'use strict';
 const { Model } = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
+module.exports = (
+  /** @type {import('sequelize').Sequelize} */ sequelize,
+  /** @type {import('sequelize').DataTypes} */ DataTypes
+) => {
   class Car extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
+     * @param {{ Review: import("sequelize").ModelStatic<Model<any, any>>; Seller: import("sequelize").ModelStatic<Model<any, any>>; }} models
      */
     static associate(models) {
       // define association here
--- models/review.js
+++ models/review.js
@@ -1,11 +1,15 @@
 'use strict';
 const { Model } = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
+module.exports = (
+  /** @type {import('sequelize').Sequelize} */ sequelize,
+  /** @type {import('sequelize').DataTypes} */ DataTypes
+) => {
   class Review extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
+     * @param {{ Car: import("sequelize").ModelStatic<Model<any, any>>; }} models
      */
     static associate(models) {
       // define association here
