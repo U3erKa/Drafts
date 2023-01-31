c7a61b4d0fd5508f09b4cbbf462b4f890d9ef7bf
Viktor Stepanov <vitya.zap@gmail.com>
2023-02-01 00:45:00 +0200

    Refresh user token on page load

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,11 @@
-import { RouteObject } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import { Chat, Home, Login, NotFound, Register } from 'pages';
+import { REFRESH_TOKEN } from './constants';
+import { refresh } from 'redux/user.slice';
+import type { RouteObject } from 'react-router-dom';
 
 export const routes: (RouteObject & { description: string })[] = [
   { path: '/', element: <Home />, description: 'Home' },
@@ -14,6 +18,17 @@ export const routes: (RouteObject & { description: string })[] = [
 const router = createBrowserRouter(routes);
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const token = localStorage.getItem(REFRESH_TOKEN);
+
+    if (token) {
+      // @ts-ignore
+      dispatch(refresh(token));
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <RouterProvider router={router} />
   );
