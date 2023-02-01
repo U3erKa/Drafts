89dfc073356add449d1f8e4767febc98037e7bc6
Viktor Stepanov <vitya.zap@gmail.com>
2023-02-01 15:03:54 +0200

    Add Socket.io for websocket

--- server/constants.ts
+++ server/constants.ts
@@ -5,3 +5,8 @@ export const DB_STRING = process.env.DB_STRING ?? 'mongodb://localhost:27017/tes
 export const SALT_OR_ROUNDS: string | number = process.env.SALT ?? 10;
 export const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET ?? 'tokensecret';
 export const ACCESS_TOKEN_TIME = process.env.ACCESS_TOKEN_TIME ?? '1d';
+
+export const SOCKET_EVENTS = {
+  NEW_MESSAGE: 'newMessage',
+  NEW_MESSAGE_ERROR: 'newMessageError',
+};
--- server/server.ts
+++ server/server.ts
@@ -1,11 +1,39 @@
 import { config as dotenv } from 'dotenv';
 dotenv();
-
 import http from 'http';
+import { Server } from 'socket.io';
+
 import app from './app';
-import { BASE_URL, PORT } from './constants';
+import Message from './db/models/message';
+import { BASE_URL, PORT, SOCKET_EVENTS } from './constants';
 
 const server = http.createServer(app);
+const io = new Server(server, { cors: { origin: '*' } });
+
+io.on('connection', (socket) => {
+  console.log('Client connected');
+
+  socket.on(SOCKET_EVENTS.NEW_MESSAGE, async (message) => {
+    try {
+      const newMessage = await (
+        await Message.create(message)
+      ).populate({
+        path: 'author',
+        select: ['-password'],
+      });
+
+      io.emit(SOCKET_EVENTS.NEW_MESSAGE, newMessage);
+    } catch (error) {
+      console.log(error);
+      socket.emit(SOCKET_EVENTS.NEW_MESSAGE_ERROR, error);
+    }
+  });
+
+  socket.on('disconnect', (reason) => {
+    console.log('Disconnected, reason:');
+    console.log(reason);
+  });
+});
 
 server.listen(PORT, () => {
   console.log(`Server started at http://${BASE_URL}:${PORT}`);
