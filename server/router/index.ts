21d89601c010491feea613e66e94f4ee928d76cc
Viktor Stepanov <vitya.zap@gmail.com>
2023-02-02 15:19:54 +0200

    Move messages route to /messages

--- server/router/index.ts
+++ server/router/index.ts
@@ -2,12 +2,14 @@ import { Router } from 'express';
 
 import { checkAccessToken } from '../middleware/token.mw';
 import authRouter from './authRouter';
+import messageRouter from './messageRouter';
 import userRouter from './userRouter';
 
 const rootRouter = Router();
 
 rootRouter.use('/users', userRouter);
 rootRouter.use('/auth', authRouter);
+rootRouter.use('/messages', checkAccessToken, messageRouter);
 
 rootRouter.get('/secret', checkAccessToken, (req, res, next) => {
   res.send('This is secret route');
--- server/router/userRouter.ts
+++ server/router/userRouter.ts
@@ -7,14 +7,10 @@ import {
   getUsers,
   updateUser,
 } from '../controller/userController';
-import { checkAccessToken } from '../middleware/token.mw';
-import messageRouter from './messageRouter';
 
 const userRouter = Router();
 
 userRouter.route('/').get(getUsers).post(createUser);
 userRouter.route('/:userId').get(getUser).put(updateUser).delete(deleteUser);
-
-userRouter.use('/:userId/messages', checkAccessToken, messageRouter);
 
 export default userRouter;
