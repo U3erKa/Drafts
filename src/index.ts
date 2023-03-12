c635c268007b81e8eb78ea6b5f2f516ae1d252ad
Viktor Stepanov <vitya.zap@gmail.com>
2023-03-12 21:26:38 +0200

    Set default options properly

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,16 @@
 #!/usr/bin/env node
-
 import commandLineArgs from 'command-line-args';
 import type { OptionDefinition } from 'command-line-args';
 
 const optionDefinitions: OptionDefinition[] = [
-  { name: 'echo', alias: 'e', type: String },
-  { name: 'verbose', alias: 'v', type: Boolean },
+  { name: 'echo', alias: 'e', type: String, defaultValue: 'Hello world!' },
+  { name: 'verbose', alias: 'v', type: Boolean, defaultValue: false },
   { name: 'src', type: String, multiple: true, defaultOption: true },
-  { name: 'timeout', alias: 't', type: Number },
+  { name: 'timeout', alias: 't', type: Number, defaultValue: 0 },
 ];
 const options = commandLineArgs(optionDefinitions, { partial: true });
 
-setTimeout(
-  () => console.log(options.echo ?? 'Hello world!'),
-  options.timeout ?? 0
-);
+setTimeout(() => console.log(options.echo), options.timeout);
 
 if (options.verbose) {
   console.log(options);
