0d13d34d965ceb5f24088e723d0d5f21886d232f
Viktor Stepanov <vitya.zap@gmail.com>
2023-04-04 09:27:50 +0300

    Add insertion sort

--- etc/js/sorting.js
+++ etc/js/sorting.js
@@ -74,3 +74,19 @@ export function selectionSort(/** @type {number[]} */ arr) {
   }
   return arr;
 }
+
+/** Good for almost sorted array\
+ * Complexity O(n^2) */
+export function insertionSort(/** @type {number[]} */ arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let current = arr[i];
+    let j = i - 1;
+
+    while (j > -1 && current < arr[j]) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    arr[j + 1] = current;
+  }
+  return arr;
+}
