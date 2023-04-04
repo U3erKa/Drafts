eb793e8feeffec06e9bf600cbf432a66f8cdf63b
Viktor Stepanov <vitya.zap@gmail.com>
2023-04-04 09:34:48 +0300

    Add shell sort

--- etc/js/sorting.js
+++ etc/js/sorting.js
@@ -142,3 +142,22 @@ export function heapSort(/** @type {number[]} */ arr) {
   }
   return arr;
 }
+
+export function shellSort(/** @type {number[]} */ arr) {
+  const len = arr.length;
+  let gap = Math.floor(len / 2);
+
+  while (gap > 0) {
+    for (let i = gap; i < len; i++) {
+      let j = i;
+      let current = arr[i];
+      while (j - gap >= 0 && current < arr[j - gap]) {
+        arr[j] = arr[j - gap];
+        j = j - gap;
+      }
+      arr[j] = current;
+    }
+    gap = Math.floor(gap / 2);
+  }
+  return arr;
+}
