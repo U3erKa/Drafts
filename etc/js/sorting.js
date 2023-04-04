85d7a6961d27216c0e38b7eebdca5e4448520cd0
Viktor Stepanov <vitya.zap@gmail.com>
2023-04-04 09:25:26 +0300

    Add gnome sort

--- etc/js/sorting.js
+++ etc/js/sorting.js
@@ -38,3 +38,22 @@ export function cocktailShakerSort(/** @type {number[]} */ arr) {
 
   return arr;
 }
+
+export function gnomeSort(/** @type {number[]} */ arr) {
+  let i = 1; // start at 1 because we're comparing i-1 to i
+  let j = 2; // start at 2 because we're comparing i-1 to i
+  while (i < arr.length) {
+    if (arr[i - 1] <= arr[i]) {
+      i = j;
+      j++;
+    } else {
+      swap(arr, i - 1, i);
+      i--;
+      if (i === 0) {
+        i = j;
+        j++;
+      }
+    }
+  }
+  return arr;
+}
