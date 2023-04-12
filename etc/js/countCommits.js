467221e5adcc099ada1b8d54625fb553ef5f0cd8
Viktor Stepanov <vitya.zap@gmail.com>
2023-04-12 17:04:14 +0300

    Add commits counter

---
+++ etc/js/countCommits.js
@@ -0,0 +1,7 @@
+const childProcess = require("child_process");
+
+const command = childProcess.exec("git log --oneline");
+let logs = "";
+
+command.stdout.on("data", (data) => (logs += data));
+command.on("exit", () => console.log(logs.split("\n").length));
