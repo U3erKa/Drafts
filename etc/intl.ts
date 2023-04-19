53e7e5f0f27a6c5afe1352fe77e782f77e6442a8
Viktor Stepanov <vitya.zap@gmail.com>
2023-04-19 20:29:36 +0300

    Add time formatter
    No date-fns or moment libs required

---
+++ etc/intl.ts
@@ -0,0 +1,37 @@
+type _DIVISORS = {
+  readonly amount: number;
+  readonly name: Intl.RelativeTimeFormatUnit;
+}[];
+
+const formatter = new Intl.RelativeTimeFormat(undefined, { numeric: "auto" });
+
+const DIVISORS: _DIVISORS = [
+  { amount: 60, name: "seconds" },
+  { amount: 60, name: "minutes" },
+  { amount: 24, name: "hours" },
+  { amount: 7, name: "days" },
+  { amount: 4.34524, name: "weeks" },
+  { amount: 12, name: "months" },
+  { amount: Number.POSITIVE_INFINITY, name: "years" },
+];
+
+/**
+ * @example
+ * const currentDate = new Date()
+ * const pastDate = new Date().setMonth(currentDate.getMonth() - 5)
+ * 
+ * console.log(formatTimeAgo(pastDate)) // "5 months ago"
+ */
+function formatTimeAgo(date: Date | number) {
+  let duration = (date.valueOf() - new Date().valueOf()) / 1000;
+
+  for (const { amount, name } of DIVISORS) {
+    if (Math.abs(duration) < amount) {
+      return formatter.format(Math.round(duration), name);
+    }
+    duration /= amount;
+  }
+  throw new RangeError(); // never reaches, for TS
+}
+
+export = { DIVISORS, formatTimeAgo };
