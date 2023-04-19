136d876066e3e7021774bdae5dc0dee318d5ea27
Viktor Stepanov <vitya.zap@gmail.com>
2023-04-19 20:43:13 +0300

    Make formatter customizable

--- etc/intl.ts
+++ etc/intl.ts
@@ -3,8 +3,6 @@ type _DIVISORS = {
   readonly name: Intl.RelativeTimeFormatUnit;
 }[];
 
-const formatter = new Intl.RelativeTimeFormat(undefined, { numeric: "auto" });
-
 const DIVISORS: _DIVISORS = [
   { amount: 60, name: "seconds" },
   { amount: 60, name: "minutes" },
@@ -19,10 +17,14 @@ const DIVISORS: _DIVISORS = [
  * @example
  * const currentDate = new Date()
  * const pastDate = new Date().setMonth(currentDate.getMonth() - 5)
- * 
+ *
  * console.log(formatTimeAgo(pastDate)) // "5 months ago"
  */
-function formatTimeAgo(date: Date | number) {
+function formatTimeAgo(date: Date | number, { lang, ...options }: Intl.RelativeTimeFormatOptions & { lang?: string }) {
+  const defaults: Intl.RelativeTimeFormatOptions = { numeric: "auto" };
+  options ? Object.assign(defaults, options) : defaults;
+
+  const formatter = new Intl.RelativeTimeFormat(lang, options);
   let duration = (date.valueOf() - new Date().valueOf()) / 1000;
 
   for (const { amount, name } of DIVISORS) {
