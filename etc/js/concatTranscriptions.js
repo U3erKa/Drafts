17d54880d3f81095ffcd7977a35fb81cdc9c14dc
Viktor Stepanov <vitya.zap@gmail.com>
2023-08-06 00:32:25 +0300

    Add script to concat transcriptions

---
+++ etc/js/concatTranscriptions.js
@@ -0,0 +1,12 @@
+const fs = require("fs")
+
+const files = fs.readdirSync(".").filter((file) => file.endsWith(".txt"))
+
+const resFile = "transcriptions.md"
+const fsOpts = { encoding: "utf8" }
+
+fs.writeFileSync(resFile, `# EAK\n\n`, fsOpts)
+files.forEach((file) => {
+  fs.appendFileSync(resFile, `## ${file}\n\n`, fsOpts)
+  fs.appendFileSync(resFile, `${fs.readFileSync(file, fsOpts)}\n\n`, fsOpts)
+})
