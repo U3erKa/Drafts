3c9dcdff174bc84189cac65869c33a53d64937ba
Viktor Stepanov <vitya.zap@gmail.com>
2023-08-06 01:55:37 +0300

    Add Intl usage examples

---
+++ etc/js/intl.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+const LANGUAGE = "uk"
+const pluralRules = new Intl.PluralRules(LANGUAGE)
+const dateTimeFormat = new Intl.DateTimeFormat(LANGUAGE, { dateStyle: "full" })
+const numberFormat = new Intl.NumberFormat(LANGUAGE)
+const listFormat = new Intl.ListFormat(LANGUAGE)
+
+const resolvedOptions = listFormat.resolvedOptions()
+
+console.log(resolvedOptions)
+
+const bread = {
+  one: "паляниця",
+  few: "паляниці",
+  many: "паляниць",
+  // other: "",
+}
+
+const breadTest = ["Паляниця", "Полуниця", "Попільниця"]
+
+for (let i = 0; i < 10; i++) {
+  const pluralRule = pluralRules.select(i)
+  // @ts-expect-error
+  console.log(numberFormat.format(i), bread[pluralRule])
+}
+
+console.log(listFormat.format(breadTest))
