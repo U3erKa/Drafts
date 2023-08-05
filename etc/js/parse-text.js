bcc17b288bf7651388cc0f6af205a33f077f77d5
Viktor Stepanov <vitya.zap@gmail.com>
2023-08-06 01:48:55 +0300

    Add some text parser

---
+++ etc/js/parse-text.js
@@ -0,0 +1,9 @@
+const fs = require("fs")
+
+const lines = fs
+  .readFileSync("text.txt", { encoding: "utf8" })
+  .split("\n")
+  .filter((line) => !line.startsWith(" "))
+
+const data = lines.map((line) => line.split("\t")[0])
+fs.writeFileSync("res.txt", data.join("\n"))
