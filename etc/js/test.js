4edce1efa7601c90c8cc637c7fbcb68b2881ca72
Viktor Stepanov <vitya.zap@gmail.com>
2023-08-06 00:32:54 +0300

    Mess with regex

---
+++ etc/js/test.js
@@ -0,0 +1,21 @@
+const fs = require("fs")
+const fsOptions = { encoding: "utf8" }
+
+const title = "Intensive Language Course English"
+const files = fs.readdirSync(".", fsOptions).filter((file) => file.includes(title))
+
+let matches = 0
+files.forEach((file) => {
+  const contents = fs.readFileSync(file, fsOptions)
+
+  const regexHell = /^Unit \n+,? [(Dialogue)|(Exercise)] \n+\.? [(Listen and repeat)|(Answers)]\.?/i
+  const res = contents.match(/Unit \d+,? ((Dialogue)|(Exercise)) \d+( Answers)?/i)
+  // contents.replace()
+
+  if (res) {
+    console.log(res)
+    matches++
+  }
+})
+
+console.log(matches)
