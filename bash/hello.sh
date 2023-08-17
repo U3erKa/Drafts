3bf1a38428aa5ed6527214fc576f34aebd1474fb
Viktor Stepanov <vitya.zap@gmail.com>
2023-08-17 10:57:01 +0300

    Add another variable example

--- bash/hello.sh
+++ bash/hello.sh
@@ -3,6 +3,13 @@
 echo "Hello world!"
 echo "Current working directory:"
 pwd
+echo ''
 
 myname="Viktor"
 echo "My name is $myname."
+echo ''
+
+mood="awesome"
+echo "Linux is $mood"
+echo "JavaScript is $mood"
+echo "Typescript is $mood"
