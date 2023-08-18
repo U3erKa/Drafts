d38c0fd48936c1e857e7dc8e2380f5ef518ab756
Viktor Stepanov <vitya.zap@gmail.com>
2023-08-18 11:58:00 +0300

    Add negative condition examples

--- bash/conditionals.sh
+++ bash/conditionals.sh
@@ -9,3 +9,9 @@ elif [ $mynum -eq 300 ]; then
 else
   echo "The condition is false"
 fi
+
+if [ ! $mynum -eq 200 ]; then
+  echo "The first negative condition is true"
+elif [ $mynum -ne 300 ]; then
+  echo "The other negative condition is true"
+fi
