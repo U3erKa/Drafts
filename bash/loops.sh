c006065cdac54f96ea0a672869b3710a09642db6
Viktor Stepanov <vitya.zap@gmail.com>
2023-08-22 20:22:10 +0300

    Add another while loop example

--- bash/loops.sh
+++ bash/loops.sh
@@ -6,3 +6,12 @@ while [ $i -lt 10 ]; do
   sleep 0.1
   i=$(($i + 1))
 done
+
+testfile=test.txt
+
+touch $testfile
+while [ -f $testfile ]; do
+  echo "Temp file exists, waiting..."
+  sleep 1
+done
+echo "Temp file is gone"
