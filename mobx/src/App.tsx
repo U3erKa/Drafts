d3e186174c4b15ec20e576f3f5e3586497e0d6cd
Viktor Stepanov <vitya.zap@gmail.com>
2023-09-19 19:48:50 +0300

    Add TodoList example that uses mobx
    Also it yells in console:
    [MobX] Since strict-mode is enabled, changing (observed)
    observable values without using an action is not allowed.
    Tried to modify: ObservableTodoStore@1.todos[..].task
    onRename	@	App.tsx:90

--- mobx/src/App.tsx
+++ mobx/src/App.tsx
@@ -1,10 +1,11 @@
 import { action, autorun, computed, makeObservable, observable } from 'mobx';
+import { observer } from 'mobx-react-lite';
 import './App.css';
 
 type Todo = {
   task: string;
   completed: boolean;
-  assignee: string | null;
+  assignee: { name: string } | null;
 };
 
 class ObservableTodoStore {
@@ -36,7 +37,10 @@ class ObservableTodoStore {
     );
   }
 
-  addTodo(task: Todo['task']) {
+  addTodo(task: Todo['task'] | null) {
+    if (!task) {
+      return;
+    }
     this.todos.push({
       task: task,
       completed: false,
@@ -49,8 +53,60 @@ const observableTodoStore = new ObservableTodoStore();
 
 console.log(observableTodoStore);
 
+function RenderCounter() {
+  return <div>{observableTodoStore.completedTodosCount}</div>;
+}
+
+// `observer` wrapper adds store to the component props
+// kinda like mapStateToProps from redux
+const TodoList = observer(({ store }: { store: ObservableTodoStore }) => {
+  const onNewTodo = () => {
+    store.addTodo(prompt('Enter a new todo:', 'coffee plz'));
+  };
+
+  return (
+    <div>
+      {store.report}
+      <ul>
+        {store.todos.map((todo, idx) => (
+          <TodoView todo={todo} key={idx} />
+        ))}
+      </ul>
+      {/* @ts-expect-error `marquee` tag */}
+      {store.pendingRequests > 0 ? <marquee>Loading...</marquee> : null}
+      <button onClick={onNewTodo}>New Todo</button>
+      <small> (double-click a todo to edit)</small>
+      <RenderCounter />
+    </div>
+  );
+});
+
+const TodoView = observer(({ todo }: { todo: Todo }) => {
+  const onToggleCompleted = () => {
+    todo.completed = !todo.completed;
+  };
+
+  const onRename = () => {
+    todo.task = prompt('Task name', todo.task) || todo.task;
+  };
+
+  return (
+    <li onDoubleClick={onRename}>
+      <input
+        type="checkbox"
+        checked={todo.completed}
+        onChange={onToggleCompleted}
+      />
+      {todo.task}
+      {todo.assignee ? <small>{todo.assignee.name}</small> : null}
+      {/* this counter goes all over the place, only when TodoView rerenders */}
+      <RenderCounter />
+    </li>
+  );
+});
+
 function App() {
-  return <div>App</div>;
+  return <TodoList store={observableTodoStore} />;
 }
 
 export default App;
