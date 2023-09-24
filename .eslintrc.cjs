1db678a0025e14dbbb6f2a5abceddf99f1c70eba
Viktor Stepanov <vitya.zap@gmail.com>
2023-09-24 19:12:22 +0300

    Update eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,6 +6,12 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
   ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname,
+  },
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
