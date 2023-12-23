e7bfb7a80e709266f10d8a8c752bfe9ba99eff23
Viktor Stepanov <vitya.zap@gmail.com>
2023-12-23 23:54:36 +0200

    feat: add makeAutoObservable usage example

--- mobx/src/store.ts
+++ mobx/src/store.ts
@@ -1,10 +1,46 @@
-import { action, autorun, computed, makeObservable, observable } from 'mobx';
+import {
+  action,
+  autorun,
+  computed,
+  makeAutoObservable,
+  makeObservable,
+  observable,
+} from 'mobx';
 
 export type Todo = {
   task: string;
   completed: boolean;
   assignee: { name: string } | null;
 };
+
+export const observableTodoStore = makeAutoObservable({
+  todos: [] as Todo[],
+  pendingRequests: 0,
+
+  get completedTodosCount() {
+    return this.todos.filter((todo: Todo) => todo.completed === true).length;
+  },
+
+  get report() {
+    if (this.todos.length === 0) return '<none>';
+    const nextTodo = this.todos.find((todo: Todo) => todo.completed === false);
+    return (
+      `Next todo: "${nextTodo ? nextTodo.task : '<none>'}". ` +
+      `Progress: ${this.completedTodosCount}/${this.todos.length}`
+    );
+  },
+
+  addTodo(task: Todo['task'] | null) {
+    if (!task) {
+      return;
+    }
+    this.todos.push({
+      task: task,
+      completed: false,
+      assignee: null,
+    });
+  },
+});
 
 export class ObservableTodoStore {
   todos: Todo[] = [];
@@ -47,5 +83,6 @@ export class ObservableTodoStore {
   }
 }
 
-export const store = new ObservableTodoStore();
+// export const store = new ObservableTodoStore();
+export const store = observableTodoStore;
 export const peopleStore = observable([{ name: 'Michel' }, { name: 'Me' }]);
