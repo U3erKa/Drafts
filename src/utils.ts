abf858dd3ca00fb990e39d0708ca82031b462934
Viktor Stepanov <vitya.zap@gmail.com>
2024-02-21 02:38:22 +0200

    feat: add promisify utility

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,19 @@
+import type { promisify as promisifyUtil } from 'util';
 import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const promisify = function promisify(func: (...args: any[]) => void) {
+  return function (...args: any) {
+    return new Promise((resolve, reject) => {
+      function callback(err: unknown, result: any) {
+        err != null ? reject(err) : resolve(result);
+      }
+      // @ts-expect-error
+      func.call(this, ...args, callback);
+    });
+  };
+} as typeof promisifyUtil;
