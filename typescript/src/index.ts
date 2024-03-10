f49288194181cfed7c3074cefd5e8569fc3a27c3
Viktor Stepanov <vitya.zap@gmail.com>
2024-03-10 11:17:50 +0200

    refactor: move node-cli

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,1 +1,16 @@
-console.log('index.ts');
+#!/usr/bin/env node
+import commandLineArgs, { type OptionDefinition } from 'command-line-args';
+
+const optionDefinitions: OptionDefinition[] = [
+  { name: 'echo', alias: 'e', type: String, defaultValue: 'Hello world!' },
+  { name: 'verbose', alias: 'v', type: Boolean, defaultValue: false },
+  { name: 'src', type: String, multiple: true, defaultOption: true },
+  { name: 'timeout', alias: 't', type: Number, defaultValue: 0 },
+];
+const options = commandLineArgs(optionDefinitions, { partial: true });
+
+setTimeout(() => console.log(options.echo), options.timeout);
+
+if (options.verbose) {
+  console.log(options);
+}
