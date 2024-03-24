a8c6b476819f7320dad736e6b563a1346d2301d0
Viktor Stepanov <vitya.zap@gmail.com>
2024-03-24 20:29:48 +0200

    feat: add uncontrolled input examples

--- nextjs/src/components/custom/HookForm.tsx
+++ nextjs/src/components/custom/HookForm.tsx
@@ -1,13 +1,17 @@
 'use client';
 
 import { useForm } from 'react-hook-form';
-import { Form, FormField } from '@/components/ui/form';
+import { Form, FormField, FormFieldProvider } from '@/components/ui/form';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
-import { defaultValues } from '@/constants';
+import { useState } from 'react';
 
 export function HookForm() {
-  const methods = useForm({ defaultValues });
+  const methods = useForm({
+    defaultValues: { firstName: '', lastName: '', age: 0 },
+  });
+  const [age, setAge] = useState(0);
+
   return (
     <Form {...methods}>
       <form onSubmit={methods.handleSubmit(console.log)}>
@@ -15,6 +19,21 @@ export function HookForm() {
           name="firstName"
           render={({ field }) => <Input {...field} />}
         />
+        <FormFieldProvider name="lastName">
+          <Input {...methods.register('lastName')} />
+        </FormFieldProvider>
+        <FormFieldProvider name="age">
+          <Input
+            value={age}
+            {...methods.register('age', {
+              valueAsNumber: true,
+              value: age,
+              onChange(e) {
+                setAge(e.target.value);
+              },
+            })}
+          />
+        </FormFieldProvider>
         <Button type="submit">Submit</Button>
       </form>
     </Form>
