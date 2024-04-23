0589d5877a04cd86988e9eb8cb5fc86354f34372
Viktor Stepanov <vitya.zap@gmail.com>
2024-04-23 22:58:52 +0300

    style: reformat

--- nodejs/src/esm/index.js
+++ nodejs/src/esm/index.js
@@ -5,22 +5,22 @@
  * @returns {Promise<T>}
  */
 export function launchWorker(workerPath, options) {
-  const { workerData, ...restOptions } = options ?? {}
+  const { workerData, ...restOptions } = options ?? {};
   return new Promise((resolve, reject) => {
-    const worker = new Worker(workerPath, restOptions)
+    const worker = new Worker(workerPath, restOptions);
     const handleMessage = (/** @type {MessageEvent} */ e) => {
-      resolve(e.data)
-      worker.terminate()
-    }
+      resolve(e.data);
+      worker.terminate();
+    };
     const handleError = (/** @type {ErrorEvent | MessageEvent} */ e) => {
-      reject(e instanceof MessageEvent ? e.data : e.error ?? e.message)
-      worker.terminate()
-    }
-    worker.addEventListener("message", handleMessage, { once: true })
-    worker.addEventListener("error", handleError, { once: true })
-    worker.addEventListener("messageerror", handleError, { once: true })
-    if (workerData) worker.postMessage(workerData)
-  })
+      reject(e instanceof MessageEvent ? e.data : e.error ?? e.message);
+      worker.terminate();
+    };
+    worker.addEventListener('message', handleMessage, { once: true });
+    worker.addEventListener('error', handleError, { once: true });
+    worker.addEventListener('messageerror', handleError, { once: true });
+    if (workerData) worker.postMessage(workerData);
+  });
 }
 
 /**
@@ -31,7 +31,7 @@ export function launchWorker(workerPath, options) {
  * @returns {[(workerData: T) => Promise<R>, () => void]}
  */
 export function createWorker(workerPath, options) {
-  const worker = new Worker(workerPath, options)
+  const worker = new Worker(workerPath, options);
   function invokeWorker(/** @type {T} */ workerData) {
     const id = crypto.randomUUID();
     worker.postMessage({ ...workerData, id });
@@ -44,31 +44,31 @@ export function createWorker(workerPath, options) {
         if (message.id === id) reject(message);
       };
       const handleMessageError = (/** @type {MessageEvent} */ e) => {
-        reject(e.data)
-      }
-      worker.addEventListener("message", handleMessage, { once: true })
-      worker.addEventListener("error", handleError, { once: true })
-      worker.addEventListener("messageerror", handleMessageError, { once: true })
-    })
+        reject(e.data);
+      };
+      worker.addEventListener('message', handleMessage, { once: true });
+      worker.addEventListener('error', handleError, { once: true });
+      worker.addEventListener('messageerror', handleMessageError, { once: true });
+    });
   }
   return [invokeWorker, () => worker.terminate()];
 }
 
 const workers = Array.from({ length: navigator.hardwareConcurrency }).map(() =>
-  createWorker("web-worker.js", { type: "module" }),
-)
+  createWorker('web-worker.js', { type: 'module' }),
+);
 async function runWorkers() {
   const messages = workers.map(([invokeWorker, terminateWorker], i) => invokeWorker({ hello: 'world', i }));
   const data = await Promise.all(messages);
   return console.log(data);
 }
-const interval = setInterval(runWorkers, 5000)
+const interval = setInterval(runWorkers, 5000);
 setTimeout(() => {
-  clearInterval(interval)
+  clearInterval(interval);
   for (const [invokeWorker, terminateWorker] of workers) {
     terminateWorker();
   }
-}, 60 * 1000)
+}, 60 * 1000);
 
 /*
 const tasks = Array.from({ length: navigator.hardwareConcurrency }).map((_, i) =>
