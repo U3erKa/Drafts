e541c2218ccddf67a1293bd24958694bf0648056
Viktor Stepanov <vitya.zap@gmail.com>
2024-04-24 21:24:32 +0300

    refactor: simplify downloadFile util

--- nodejs/src/downloadFile.ts
+++ nodejs/src/downloadFile.ts
@@ -1,28 +1,9 @@
-import axios from 'axios';
-import { promises as fs, createWriteStream } from 'fs';
+import { promises as fs } from 'fs';
 
-export async function downloadFile(url: string, outFolder = '../dist', filename?: string) {
-  const outputLocationPath = new URL(
-    `${outFolder}/${filename ?? url.split('/').at(-1)}`,
-    import.meta.url,
-  );
+export async function downloadFile(url: string, filename?: string, outFolder = '../dist') {
+  const outputFilePath = new URL(`${outFolder}/${filename ?? url.split('/').at(-1)}`, import.meta.url);
+  const data = await fetch(url).then((r) => r.arrayBuffer());
 
-  await fs.writeFile(outputLocationPath, '');
-  const writer = createWriteStream(outputLocationPath);
-  const { data } = await axios.get(url, { url, responseType: 'stream' });
-
-  return new Promise<URL>((resolve, reject) => {
-    data.pipe(writer);
-    let error: Error;
-    writer.on('error', (err) => {
-      error = err;
-      writer.close();
-      reject(err);
-    });
-    writer.on('close', () => {
-      if (!error) {
-        resolve(outputLocationPath);
-      }
-    });
-  });
+  await fs.writeFile(outputFilePath, Buffer.from(data));
+  return outputFilePath;
 }
