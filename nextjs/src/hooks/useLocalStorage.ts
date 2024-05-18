d0c10e9baadedd6fade4bdc68d4ddd7cc815c348
Viktor Stepanov <vitya.zap@gmail.com>
2024-05-18 23:02:51 +0300

    fix: call listeners for necessary components only in useLocalStorage

--- nextjs/src/hooks/useLocalStorage.ts
+++ nextjs/src/hooks/useLocalStorage.ts
@@ -1,20 +1,13 @@
 import { useSyncExternalStore } from 'react';
 
-let listeners: (() => void)[] = [];
-
-function subscribe(listener: () => void) {
-  listeners = [...listeners, listener];
-  return () => {
-    listeners = listeners.filter((l) => l !== listener);
-  };
-}
+let listeners: { [key: string]: (() => void)[] } = {};
 
 function getServerSnapshot() {
   return null;
 }
 
-function onStoreChange() {
-  for (const listener of listeners) {
+function onStoreChange(key: string) {
+  for (const listener of listeners[key]) {
     listener();
   }
 }
@@ -22,6 +15,13 @@ function onStoreChange() {
 export function useLocalStorage<T = unknown>(key: string) {
   const rawValue = useSyncExternalStore(subscribe, getSnapshot, getServerSnapshot);
   const value: T | null = rawValue === null ? rawValue : JSON.parse(rawValue);
+
+  function subscribe(listener: () => void) {
+    listeners = { ...listeners, [key]: [...(listeners[key] ?? []), listener] };
+    return () => {
+      listeners = { ...listeners, [key]: listeners[key].filter((l) => l !== listener) };
+    };
+  }
 
   function getSnapshot() {
     return localStorage.getItem(key);
@@ -32,7 +32,7 @@ export function useLocalStorage<T = unknown>(key: string) {
     const newValue = value != null ? JSON.stringify(value) : (value as null | undefined);
     if (newValue == null) localStorage.removeItem(key);
     else localStorage.setItem(key, newValue);
-    if (oldValue !== newValue) onStoreChange();
+    if (oldValue !== newValue) onStoreChange(key);
   }
 
   return [value, setValue] as const;
