3ecdb6e9b5bad3cb57a854afb406e20c9284ab1e
Viktor Stepanov <vitya.zap@gmail.com>
2024-07-17 10:52:35 +0300

    chore: update vite; add react compiler

--- react/vite.config.ts
+++ react/vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import browserslistToEsbuild from 'browserslist-to-esbuild';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: { target: browserslistToEsbuild() },
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react({
+      babel: {
+        plugins: [['babel-plugin-react-compiler', { compilationMode: 'all', panicThreshold: 'CRITICAL_ERRORS' }]],
+      },
+    }),
+    tsconfigPaths(),
+  ],
 });
